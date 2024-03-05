d6631dcd3926977dc527352030b99ce82ac95f12
arm <amccarthy9904@gmail.com>
2024-03-05 10:30:47 -0800

    adds console log for image

--- src/components/Game.js
+++ src/components/Game.js
@@ -76,6 +76,7 @@ const Game = () => {
   };
 
   const onGuess = () => {
+    console.log(currCountryImg)
     let input = userInput.replace(/[^a-zA-Z' ô().üç]+/g, '').toLowerCase()
     console.log('sani input:', input)
 
